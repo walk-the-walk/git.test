d65c18977002e30c94f6680d8ea9e0416d728037
walk-the-walk <75371116+walk-the-walk@users.noreply.github.com>
2022-07-18 13:21:35 +0800

    pull test

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test 111");
         System.out.println("push test 222");
+        System.out.println("pull test");
     }
 }
